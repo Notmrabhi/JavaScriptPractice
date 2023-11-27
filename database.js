864fc3da161078fdd6de5cf95c9bb943868be668
Abhishek anand <110781004+Notmrabhi@users.noreply.github.com>
2023-11-27 20:30:17 +0000

    database

---
+++ database.js
@@ -0,0 +1,12 @@
+"use strict"
+
+let a = "start" //string
+let b = 23456 //number
+let c = true //boolean
+
+console.log(typeof null)
+
+// bigint for big numbers
+// null --->objectnumber
+// undefine 
+
---
+++ variable.js
@@ -0,0 +1,6 @@
+const idNumber = 432455
+let a = "abhishel@546"
+var c = "1234"
+let d;
+
+console.table([idNumber, a, c, d])

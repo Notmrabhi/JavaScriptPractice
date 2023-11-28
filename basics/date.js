afa23b66bfb3d9907918ae2621371600cdc92c90
Abhishek anand <110781004+Notmrabhi@users.noreply.github.com>
2023-11-28 06:36:11 +0000

    date

---
+++ basics/date.js
@@ -0,0 +1,8 @@
+//date
+
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.toLocaleDateString())
+console.log(myDate.getDate())
+console.log(myDate.getTime())
+console.log(myDate.toLocaleTimeString())
